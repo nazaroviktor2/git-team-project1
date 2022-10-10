39b4ed610014e49ac674558159319a8a53b776fc
nazaroviktor2 <nazarov20012004@gmail.com>
2022-10-10 21:51:17 +0300

    Инициализация проекта

--- main.py
+++ main.py
@@ -5,12 +5,12 @@ from functions.file import save_text_to_file
 
 if __name__ == '__main__':
     commands = {
-                "0": "Список команд",
-                "1": 'Шифрование методом Цезаря',
-                }
+        "0": "Список команд",
+        "1": 'Шифрование методом Цезаря',
+    }
     print("Список команд: ")
-    for name, text in commands.items():
-        print("{0} - {1}".format(name, text))
+    for name_com, text_com in commands.items():
+        print("{0} - {1}".format(name_com, text_com))
     user_input = input('Введите номер команды из списка, чтобы запустить ее (q to exit): ')
     while True:
         if user_input == "q":
@@ -23,14 +23,14 @@ if __name__ == '__main__':
 
         elif user_input == "1":
             print("Введите текст для шифрования (поддерживает только англиский язык)")
-            text = input()
+            text_for_encrypt = input()
             print("Введите ключ для шифрование (число)")
             try:
                 key = int(input())
             except ValueError:
                 print("Введен не верный ключ! Ключем может быть любое число ")
             else:
-                encrypt_text = encrypt_by_tsezar(text, key)
+                encrypt_text = encrypt_by_tsezar(text_for_encrypt, key)
                 print("Зашифрованный текст: ")
                 print(encrypt_text)
                 file_name = input("Для сохрание текста в файл, ввидете имя файла иначе нажмите enter: ")
@@ -40,8 +40,4 @@ if __name__ == '__main__':
         else:
             print("Не известная команда")
 
-
-
         user_input = input('Введите номер команды из списка, чтобы запустить ее (q to exit): ')
-
-
